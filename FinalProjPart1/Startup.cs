8a13e0d584855aa39011aefece22cc27d72cab3e
mayankraval <mayankraval@hotmail.com>
2016-08-05 18:11:15 -0400

    initial Project structure

---
+++ FinalProjPart1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProjPart1.Startup))]
+namespace FinalProjPart1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
